fa7a703b5bcfb64ebf444333e43340d2815048ea
gigiocode <89426859+gigiocode@users.noreply.github.com>
2023-05-17 19:47:31 -0300

    Ajuste das cores CSS

--- src/app/components/menu.component.ts
+++ src/app/components/menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'menu-component',
-  template: '<h2>Giovane</h2>',
+  template: '<h2>Olá, Giovane!</h2>',
   styles: ['h2{color: dark}']
 
 })
